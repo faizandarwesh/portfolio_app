ff3407be5777017e9e05abb2aa7a15e03e91ae59
FaizanGT <faizan.darwesh@genetechsolutions.com>
2024-09-09 16:34:40 +0500

    Some firebase hosting related file changes

--- lib/utils/theme/theme_config.dart
+++ lib/utils/theme/theme_config.dart
@@ -1,21 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+class ThemeConfig{
 
-// Create ChangeNotifierProvider for ThemeNotifier
-final themeProvider = ChangeNotifierProvider<ThemeNotifier>((ref) {
-  return ThemeNotifier();
-});
-
-
-class ThemeNotifier extends ChangeNotifier {
-  bool _isDarkMode = false;
-
-  bool get isDarkMode => _isDarkMode;
-
-  // Toggles between light and dark theme
-  void toggleTheme() {
-    _isDarkMode = !_isDarkMode;
-    notifyListeners();  // Notify listeners that theme has changed
-  }
+  final ThemeData lightTheme = ThemeData.light();
+  final ThemeData darkTheme = ThemeData.dark();
 }
