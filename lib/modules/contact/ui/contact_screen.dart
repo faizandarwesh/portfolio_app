e2bbd9d4cfa31f4259536cccf22692b4c4716f5b
FaizanGT <faizan.darwesh@genetechsolutions.com>
2024-09-23 13:52:05 +0500

    Add animation in contact screen and remove contact form

--- lib/modules/contact/ui/contact_screen.dart
+++ lib/modules/contact/ui/contact_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:email_validator/email_validator.dart';
-import 'package:flutter_email_sender/flutter_email_sender.dart';
+import 'package:lottie/lottie.dart';
 import '../../../utils/theme/social_icons_widget.dart';
 
 class ContactScreen extends StatefulWidget {
@@ -11,41 +10,6 @@ class ContactScreen extends StatefulWidget {
 }
 
 class _ContactScreenState extends State<ContactScreen> {
-  final _formKey = GlobalKey<FormState>();
-  final TextEditingController _nameController = TextEditingController();
-  final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _messageController = TextEditingController();
-
-  bool _isSending = false;
-
-  Future<void> _sendEmail() async {
-    if (_formKey.currentState!.validate()) {
-      setState(() {
-        _isSending = true;
-      });
-
-      final email = Email(
-        body: _messageController.text,
-        subject: 'Contact Form Message from ${_nameController.text}',
-        recipients: ['yourEmail@example.com'], // Add your email here
-      );
-
-      try {
-        await FlutterEmailSender.send(email);
-       /* ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(content: Text('Message sent successfully!')),
-        );*/
-      } catch (error) {
-      /*  ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(content: Text('Failed to send message. Please try again.')),
-        );*/
-      } finally {
-        setState(() {
-          _isSending = false;
-        });
-      }
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -73,98 +37,21 @@ class _ContactScreenState extends State<ContactScreen> {
                   fontFamily: 'Poppins',
                   fontWeight: FontWeight.w700),
             ),
-            Form(
-              key: _formKey,
-              child: Column(
-                children: [
-                  const SizedBox(height: 16),
-                  Text(
-                    'Get in Touch',
-                    style: theme.textTheme.headlineSmall!.copyWith(
-                      fontWeight: FontWeight.bold,
-                      color: theme.primaryColor,
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  const SocialIconsWidget(),
-                  const SizedBox(height: 32),
-                  TextFormField(
-                    controller: _nameController,
-                    decoration: InputDecoration(
-                      labelText: 'Full Name',
-                      prefixIcon: const Icon(Icons.person),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                    ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter your name';
-                      }
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: 20),
-                  TextFormField(
-                    controller: _emailController,
-                    decoration: InputDecoration(
-                      labelText: 'Email Address',
-                      prefixIcon: const Icon(Icons.email),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                    ),
-                    keyboardType: TextInputType.emailAddress,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter your email';
-                      } else if (!EmailValidator.validate(value)) {
-                        return 'Please enter a valid email';
-                      }
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: 20),
-                  TextFormField(
-                    controller: _messageController,
-                    maxLines: 6,
-                    decoration: InputDecoration(
-                      labelText: 'Tell us about your project',
-                      prefixIcon: const Icon(Icons.message),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                    ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter your message';
-                      }
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: 30),
-                  SizedBox(
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      onPressed: _isSending ? null : _sendEmail,
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: theme.primaryColor,
-                        padding: const EdgeInsets.symmetric(vertical: 16),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                      ),
-                      child: _isSending
-                          ? const CircularProgressIndicator(color: Colors.white)
-                          : const Text(
-                        'Send message',
-                        style: TextStyle(fontSize: 16,color: Colors.white),
-                      ),
-                    ),
-                  ),
-                ],
+            const SizedBox(height: 16),
+            Text(
+              'Get in Touch',
+              style: theme.textTheme.headlineSmall!.copyWith(
+                fontWeight: FontWeight.bold,
+                color: theme.primaryColor,
               ),
             ),
+            const SizedBox(height: 20),
+            Center(
+              child: Lottie.asset('assets/raw/contact_animation.json',
+              ),
+            ),
+            const SizedBox(height: 20),
+            const SocialIconsWidget(),
           ],
         ),
       ),
