4964f088ce042ddded8e92363f3d6248ef01b8e3
FaizanGT <faizan.darwesh@genetechsolutions.com>
2024-09-23 10:00:59 +0500

    add timeline in journey screen

--- lib/modules/journey/ui/journey_listing_screen.dart
+++ lib/modules/journey/ui/journey_listing_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 import 'package:portfolio_app/modules/journey/ui/journey_card_widget.dart';
+import 'package:timelines/timelines.dart';
 import '../../../data/data.dart';
 
 class JourneyListingScreen extends StatelessWidget {
@@ -31,24 +32,75 @@ class JourneyListingScreen extends StatelessWidget {
                 height: 32,
               ),
               Center(
-                child: Lottie.asset('assets/raw/working_developer.json',
+                child: Lottie.asset('assets/raw/mobile_developer.json',
                     height: 350),
               ),
               const SizedBox(
                 height: 16,
               ),
-              ListView.builder(
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                itemCount: journey.length,
-                itemBuilder: (BuildContext context, int index) {
-                  final data = journey[index];
+              // Timeline.tileBuilder(
+              //   shrinkWrap: true,
+              //   builder: TimelineTileBuilder.fromStyle(
+              //     contentsAlign: ContentsAlign.alternating,
+              //     contentsBuilder: (context, index) => Padding(
+              //       padding: const EdgeInsets.all(24.0),
+              //       child: Text('Timeline Event $index'),
+              //     ),
+              //     itemCount: 10,
+              //   ),),
+          FixedTimeline.tileBuilder(
+            builder: TimelineTileBuilder.connectedFromStyle(
+              contentsAlign: ContentsAlign.reverse,
+              oppositeContentsBuilder: (context, index) {
+                final role = journey[index]['role'];
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    role,
+                    style: const TextStyle(
+                        fontSize: 16,
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w400)
+                  ),
+                );
+              },
+              contentsBuilder: (context, index) {
+                final title = journey[index]['title'];
+                return Card(
+                  elevation: 2,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Text(
+                      title,
+                      style: const TextStyle(
+                          fontSize: 18,
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.w700)
+                    ),
+                  ),
+                );
+              },
+              connectorStyleBuilder: (context, index) => ConnectorStyle.solidLine,
+              indicatorStyleBuilder: (context, index) => IndicatorStyle.dot,
+              itemCount: journey.length,
+            ),
+          ),
 
-                  return JourneyCardWidget(
-                    journey: data,
-                  );
-                },
-              )
+          // ListView.builder(
+                //   shrinkWrap: true,
+                //   physics: const NeverScrollableScrollPhysics(),
+                //   itemCount: journey.length,
+                //   itemBuilder: (BuildContext context, int index) {
+                //     final data = journey[index];
+                //
+                //     return JourneyCardWidget(
+                //       journey: data,
+                //     );
+                //   },
+                // )
             ],
           ),
         ),
